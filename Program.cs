3255dd54ebb1c5104f8df94e5c8c8f1806029ca3
moerwald <andreas.mewald@gmail.com>
2019-11-25 07:56:32 +0000

    Added example for seconds since Unix epoch

--- Program.cs
+++ Program.cs
@@ -2,12 +2,22 @@ using System;
 
 namespace playground_csharp_datetime
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
             // Create date time string, where each segment is seperated by "_"
             Console.WriteLine(DateTime.UtcNow.ToString("o").Replace(":", "_").Replace(".", "_").Replace("-", "_"));
+
+            // Convert to seconds since Unix epoch
+            Console.WriteLine(ConvertToUnixTimestamp(DateTime.UtcNow));
+        }
+
+        public static double ConvertToUnixTimestamp(DateTime date)
+        {
+            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            TimeSpan diff = date.ToUniversalTime() - origin;
+            return Math.Floor(diff.TotalSeconds);
         }
     }
 }
